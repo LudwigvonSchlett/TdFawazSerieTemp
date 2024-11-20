4718ace2a07d2457964e67f3f47e5776106b377a
SENECHAL Louis <130775686+LudwigvonSchlett@users.noreply.github.com>
2024-11-20 10:16:34 +0100

    initial commit

---
+++ TimeSeriesGenerator.cpp
@@ -0,0 +1,5 @@
+//
+// Created by ludwig_van_schlett on 20/11/24.
+//
+
+#include "TimeSeriesGenerator.h"
---
+++ TimeSeriesGenerator.h
@@ -0,0 +1,16 @@
+//
+// Created by ludwig_van_schlett on 20/11/24.
+//
+
+#ifndef TIMESERIESGENERATOR_H
+#define TIMESERIESGENERATOR_H
+
+
+
+class TimeSeriesGenerator {
+
+};
+
+
+
+#endif //TIMESERIESGENERATOR_H
